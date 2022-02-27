28bde8ca4b53af1507c5bf0727528a0d16717da0
Evan Cooke <evncooke@gmail.com>
2022-02-27 06:23:11 -0600

    Added todo list

--- src/GameOfLifeDriver.java
+++ src/GameOfLifeDriver.java
@@ -1,3 +1,7 @@
+//TODO:
+//1. allow shapes to travel across borders onto the other side
+//2. add functions to create these shapes at a starting point: https://dmackinnon1.github.io/svgpixel/seeds.html?
+
 public class GameOfLifeDriver {
     public static void main(String[] args) {
         new MainFrame();
